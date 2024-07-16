2d75b261a1c34768da0192f52bf11f852308bd10
jiunshinn <jiun.dev@gmail.com>
2024-07-16 20:11:59 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by jiun Shin on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
